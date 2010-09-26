419741ad6921cb4ace490200d6f17dcba36c500f
Jim Gay <jim@saturnflyer.com>
2010-09-25 23:27:51 -0400

    allow spec to pass when run with the entire test suite (loading test extensions)

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,5 +2,5 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Page do
   its(:default_child){ should == Page}
-  its(:allowed_children){ should == [Page, ArchivePage, FileNotFoundPage]}
+  its(:allowed_children){ should == [Page, *Page.descendants.sort_by(&:name)].select(&:in_menu?).reject{|p| p.name =~ /Archive(Day|Month|Year)IndexPage/}}
 end
